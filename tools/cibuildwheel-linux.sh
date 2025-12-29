9ed41dcb959bb9a4a2fa26af27c6309aab959a94
Andy Shapiro <shapiromatron@gmail.com>
2025-12-28 20:01:03 -0500

    update

--- tools/cibuildwheel-linux.sh
+++ tools/cibuildwheel-linux.sh
@@ -7,5 +7,9 @@ yum install -y cmake gsl-devel eigen3-devel
 
 cp ./vendor/nlopt-2.7.1.tar.gz ~
 cd ~
-tar -xf nlopt-2.7.1.tar.gz && cd nlopt-2.7.1 && mkdir build && cd build && cmake -DBUILD_SHARED_LIBS=OFF .. && make install
+tar -xf nlopt-2.7.1.tar.gz && cd nlopt-2.7.1
+ls -laht # TODO - remove temp
+sed -i 's/cmake_minimum_required (VERSION 3\.2)/cmake_minimum_required (VERSION 3.15)/' CMakeLists.txt
+head -n 20 CMakeLists.txt # TODO - remove temp
+mkdir build && cd build && cmake -DBUILD_SHARED_LIBS=OFF .. && make install
 cd $GITHUB_WORKSPACE
