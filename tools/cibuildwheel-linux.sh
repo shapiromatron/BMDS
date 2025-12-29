9e9d37c9bf9008082aa80d3f3452a1d41204865a
Andy Shapiro <shapiromatron@gmail.com>
2025-12-28 20:10:22 -0500

    updates

--- tools/cibuildwheel-linux.sh
+++ tools/cibuildwheel-linux.sh
@@ -10,6 +10,8 @@ cd ~
 tar -xf nlopt-2.7.1.tar.gz && cd nlopt-2.7.1
 ls -laht # TODO - remove temp
 sed -i 's/cmake_minimum_required (VERSION 3\.2)/cmake_minimum_required (VERSION 3.15)/' CMakeLists.txt
+sed -i 's/cmake_minimum_required (VERSION 3\.2)/cmake_minimum_required (VERSION 3.15)/' cmake/generate-cpp.cmake
 head -n 20 CMakeLists.txt # TODO - remove temp
+head -n 20 cmake/generate-cpp.cmake # TODO - remove temp
 mkdir build && cd build && cmake -DBUILD_SHARED_LIBS=OFF .. && make install
 cd $GITHUB_WORKSPACE
